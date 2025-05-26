cf698e8b59158342e48ab64031652c17bff68102
Mahir Faysal Tushar <www.mahirfaysaltushar@gmail.com>
2025-05-26 12:47:21 +0600

    Add files via upload

---
+++ Chapter 1 - Introduction to C++/2.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using std::cout;
+
+int main()
+{
+//      cout << "THis is "
+        cout << "the first"; /* A program with multiple outputs
+        cout << " trap"; /* That’s the last one. */
+        return 0;
+}
