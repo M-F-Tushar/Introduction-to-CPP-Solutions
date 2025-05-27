00cb12c025e9ae2e77f969b8e951d542d9519a69
Mahir Faysal Tushar <www.mahirfaysaltushar@gmail.com>
2025-05-27 20:37:43 +0600

    Add files via upload

---
+++ Chapter 4 - Operators/Example/4.8.cpp
@@ -0,0 +1,9 @@
+#include <iostream> // Example 4.8
+int main()
+{
+    char c;
+    int i;
+    double d;
+    std::cout << sizeof(c) << ' ' << sizeof(i) << ' ' << sizeof(d) << '\n';
+    return 0;
+}
