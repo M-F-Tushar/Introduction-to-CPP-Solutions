80c4aa5254662a858aeffac4984c9b38eb37ed65
Mahir Faysal Tushar <www.mahirfaysaltushar@gmail.com>
2025-06-02 12:08:53 +0600

    Add files via upload

---
+++ Chapter 7 - Arrays/Example/7.3.cpp
@@ -0,0 +1,11 @@
+#include <iostream> // Example 7.3
+int main()
+{
+    int i, a[3];
+    for (i = 0; i < 4; i++)
+    {
+        a[i] = 0;
+        std::cout << a[i] << '\n';
+    }
+    return 0;
+}
