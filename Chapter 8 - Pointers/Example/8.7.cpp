c3d8e6bb28a041a6b84762a2ae63e73872490a59
Mahir Faysal Tushar <www.mahirfaysaltushar@gmail.com>
2025-06-06 11:35:17 +0600

    Update 8.7.cpp

--- Chapter 8 - Pointers/Example/8.7.cpp
+++ Chapter 8 - Pointers/Example/8.7.cpp
@@ -5,7 +5,7 @@ int main()
     char s[] = "abcd";
     int i = 10;
     ptr = &i;
-    //*ptr += 20;        // Wrong, cast is needed.
+    //*ptr += 20;      // Wrong, cast is needed.
     *(int *)ptr += 20; // That's ok now.
     std::cout << i << '\n';
     ptr = s + 2; // ptr points to the s[2] element with value 'c'. 
